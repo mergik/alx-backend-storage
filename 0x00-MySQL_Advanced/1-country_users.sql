eb6a43e46d2132d26e3e61b2d1d33d0be03ecd64
mergik <gmerlix@yahoo.com>
2024-01-17 19:33:05 +0300

    2. Best band ever!

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
--- SQL script that creates a table `users`
+-- Creates a table `users`
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
